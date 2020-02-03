3f60a4acbd16df8a9fe36c842104f239079ef0f0
Christine Suehiro <csuehiro@student.42sp.org.br>
2020-02-03 20:50:43 -0300

    improvement

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: csuehiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 15:48:11 by csuehiro          #+#    #+#             */
-/*   Updated: 2020/01/31 21:03:07 by csuehiro         ###   ########.fr       */
+/*   Updated: 2020/02/03 19:33:55 by csuehiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char	*auxsrc;
 
 	cont = 0;
-	auxdst = (char *)dst;
-	auxsrc = (char *)src;
 	if (dst == 0 && src == 0)
 		return (0);
+	auxdst = (char *)dst;
+	auxsrc = (char *)src;
 	while (cont < (int)n)
 	{
 		auxdst[cont] = auxsrc[cont];
